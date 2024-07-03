0e244bada38c32fd5b31f6b1867dec53f2588332
mohammed893 <pspcpspc7@gmail.com>
2024-07-03 05:10:47 +0300

    [ADD] basics

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+
+async function startServer() {
+//   await pool.connect().then(
+//     () => { console.log('Connected to PostgreSQL database'); }
+//   ).catch((err) => {
+//     console.error('Error connecting to PostgreSQL:', err.stack);
+//     process.exit(1);
+//   });
+
+  server.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT} !`);
+  });
+}
+
+startServer();
