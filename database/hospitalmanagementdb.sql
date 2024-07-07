24631ee4b3cd4594d7cde485d7929eb8a7877a49
mohammed893 <pspcpspc6@gmail.com>
2024-07-08 00:52:23 +0300

    [EDT]Creating DB before Creating Tables ✨

--- database/hospitalmanagementdb.sql
+++ database/hospitalmanagementdb.sql
@@ -1,7 +1,4 @@
---
 -- PostgreSQL database dump
---
-
 
 -- Dumped from database version 16.3
 -- Dumped by pg_dump version 16.0
@@ -21,6 +18,10 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
+-- Create the database
+CREATE DATABASE project055;
+\connect project055;
+
 --
 -- Name: appointments; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -37,7 +38,6 @@ CREATE TABLE public.appointments (
     notes text,
     slot_id integer
 );
-
 
 ALTER TABLE public.appointments OWNER TO postgres;
 
@@ -56,7 +56,6 @@ CREATE TABLE public.doctors (
     working_hours jsonb
 );
 
-
 ALTER TABLE public.doctors OWNER TO postgres;
 
 --
@@ -70,7 +69,6 @@ CREATE SEQUENCE public.old_time_slots_old_slot_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.old_time_slots_old_slot_id_seq OWNER TO postgres;
 
@@ -87,7 +85,6 @@ CREATE TABLE public.old_time_slots (
     is_available boolean,
     deletion_date date
 );
-
 
 ALTER TABLE public.old_time_slots OWNER TO postgres;
 
@@ -108,7 +105,6 @@ CREATE TABLE public.patients (
     patient_id integer NOT NULL
 );
 
-
 ALTER TABLE public.patients OWNER TO postgres;
 
 --
@@ -122,7 +118,6 @@ CREATE SEQUENCE public.promotions_promotionid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.promotions_promotionid_seq OWNER TO postgres;
 
@@ -140,7 +135,6 @@ CREATE TABLE public.promotions (
     newsalary numeric(10,2)
 );
 
-
 ALTER TABLE public.promotions OWNER TO postgres;
 
 --
@@ -154,7 +148,6 @@ CREATE SEQUENCE public.raises_raiseid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.raises_raiseid_seq OWNER TO postgres;
 
@@ -170,7 +163,6 @@ CREATE TABLE public.raises (
     dateofraise date DEFAULT CURRENT_DATE
 );
 
-
 ALTER TABLE public.raises OWNER TO postgres;
 
 --
@@ -184,7 +176,6 @@ CREATE SEQUENCE public.requests_requestid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.requests_requestid_seq OWNER TO postgres;
 
@@ -203,7 +194,6 @@ CREATE TABLE public.requests (
     requesttype text NOT NULL
 );
 
-
 ALTER TABLE public.requests OWNER TO postgres;
 
 --
@@ -217,7 +207,6 @@ CREATE SEQUENCE public.staff_staffid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.staff_staffid_seq OWNER TO postgres;
 
@@ -243,7 +232,6 @@ CREATE TABLE public.staff (
     employment_status character varying(50)
 );
 
-
 ALTER TABLE public.staff OWNER TO postgres;
 
 --
@@ -257,7 +245,6 @@ CREATE SEQUENCE public.time_slots_slotid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.time_slots_slotid_seq OWNER TO postgres;
 
@@ -275,7 +262,6 @@ CREATE TABLE public.time_slots (
     available_days jsonb
 );
 
-
 ALTER TABLE public.time_slots OWNER TO postgres;
 
 --
@@ -289,7 +275,6 @@ CREATE SEQUENCE public.vacations_vacationid_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE public.vacations_vacationid_seq OWNER TO postgres;
 
@@ -306,7 +291,6 @@ CREATE TABLE public.vacations (
     typeofvacation character varying(20)
 );
 
-
 ALTER TABLE public.vacations OWNER TO postgres;
 
 --
@@ -316,14 +300,12 @@ ALTER TABLE public.vacations OWNER TO postgres;
 COPY public.appointments (appointment_id, doctor_id, patient_id, appointment_date, start_time, end_time, status, type, notes, slot_id) FROM stdin;
 \.
 
-
 --
 -- Data for Name: doctors; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.doctors (doctor_id, firstname, qualification, specialization, experience, email, phone, working_hours) FROM stdin;
 \.
-
 
 --
 -- Data for Name: old_time_slots; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -332,14 +314,12 @@ COPY public.doctors (doctor_id, firstname, qualification, specialization, experi
 COPY public.old_time_slots (old_slot_id, doctor_id, date, start_time, end_time, is_available, deletion_date) FROM stdin;
 \.
 
-
 --
 -- Data for Name: patients; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.patients (firstname, secondname, nationalidnumber, dateofbirth, gender, email, phonenumber, address, medical_history, patient_id) FROM stdin;
 \.
-
 
 --
 -- Data for Name: promotions; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -348,14 +328,12 @@ COPY public.patients (firstname, secondname, nationalidnumber, dateofbirth, gend
 COPY public.promotions (promotionid, staffid, previousdegree, newdegree, promotiondate, previoussalary, newsalary) FROM stdin;
 \.
 
-
 --
 -- Data for Name: raises; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.raises (staffid, raiseid, newsalary, raise, dateofraise) FROM stdin;
 \.
-
 
 --
 -- Data for Name: requests; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -364,14 +342,12 @@ COPY public.raises (staffid, raiseid, newsalary, raise, dateofraise) FROM stdin;
 COPY public.requests (requestid, staffid, receiver_role, receiver_name, content, status, dateofrequest, requesttype) FROM stdin;
 \.
 
-
 --
 -- Data for Name: staff; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.staff (staffid, firstname, lastname, gender, email, phone_number, address, nationalidnumber, jobtitle, department, dateofhire, dateofbirth, salary, managerid, employment_status) FROM stdin;
 \.
-
 
 --
 -- Data for Name: time_slots; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -380,7 +356,6 @@ COPY public.staff (staffid, firstname, lastname, gender, email, phone_number, ad
 COPY public.time_slots (slot_id, doctor_id, date, start_time, end_time, is_available, available_days) FROM stdin;
 \.
 
-
 --
 -- Data for Name: vacations; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -388,13 +363,11 @@ COPY public.time_slots (slot_id, doctor_id, date, start_time, end_time, is_avail
 COPY public.vacations (vacationid, staffid, startdate, enddate, duration, typeofvacation) FROM stdin;
 \.
 
-
 --
 -- Name: old_time_slots_old_slot_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.old_time_slots_old_slot_id_seq', 1, false);
-
 
 --
 -- Name: promotions_promotionid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
@@ -402,13 +375,11 @@ SELECT pg_catalog.setval('public.old_time_slots_old_slot_id_seq', 1, false);
 
 SELECT pg_catalog.setval('public.promotions_promotionid_seq', 1, false);
 
-
 --
 -- Name: raises_raiseid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.raises_raiseid_seq', 1, false);
-
 
 --
 -- Name: requests_requestid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
@@ -416,13 +387,11 @@ SELECT pg_catalog.setval('public.raises_raiseid_seq', 1, false);
 
 SELECT pg_catalog.setval('public.requests_requestid_seq', 1, false);
 
-
 --
 -- Name: staff_staffid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.staff_staffid_seq', 1, false);
-
 
 --
 -- Name: time_slots_slotid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
@@ -430,191 +399,139 @@ SELECT pg_catalog.setval('public.staff_staffid_seq', 1, false);
 
 SELECT pg_catalog.setval('public.time_slots_slotid_seq', 1, false);
 
-
 --
 -- Name: vacations_vacationid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.vacations_vacationid_seq', 1, false);
 
+--
+-- Name: appointments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
 
---
--- Name: appointments appointments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.appointments
     ADD CONSTRAINT appointments_pkey PRIMARY KEY (appointment_id);
 
-
 --
--- Name: doctors doctors_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: doctors_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.doctors
     ADD CONSTRAINT doctors_pkey PRIMARY KEY (doctor_id);
 
-
 --
--- Name: old_time_slots old_time_slots_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: old_time_slots_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.old_time_slots
     ADD CONSTRAINT old_time_slots_pkey PRIMARY KEY (old_slot_id);
 
-
 --
--- Name: promotions promotions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.promotions
-    ADD CONSTRAINT promotions_pkey PRIMARY KEY (promotionid);
-
-
---
--- Name: raises raises_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.raises
-    ADD CONSTRAINT raises_pkey PRIMARY KEY (raiseid);
-
-
---
--- Name: requests requests_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.requests
-    ADD CONSTRAINT requests_pkey PRIMARY KEY (requestid);
-
-
---
--- Name: staff staff_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.staff
-    ADD CONSTRAINT staff_pkey PRIMARY KEY (staffid);
-
-
---
--- Name: vacations vacations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.vacations
-    ADD CONSTRAINT vacations_pkey PRIMARY KEY (vacationid);
-
-
---
--- Name: staff staff_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.staff
-    ADD CONSTRAINT staff_email_key UNIQUE (email);
-
-
---
--- Name: patients patients_nationalidnumber_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.patients
-    ADD CONSTRAINT patients_nationalidnumber_key UNIQUE (nationalidnumber);
-
-
---
--- Name: patients patients_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: patients_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.patients
     ADD CONSTRAINT patients_pkey PRIMARY KEY (patient_id);
 
+--
+-- Name: promotions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.promotions
+    ADD CONSTRAINT promotions_pkey PRIMARY KEY (promotionid);
 
 --
--- Name: time_slots time_slots_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: raises_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.raises
+    ADD CONSTRAINT raises_pkey PRIMARY KEY (raiseid);
+
+--
+-- Name: requests_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.requests
+    ADD CONSTRAINT requests_pkey PRIMARY KEY (requestid);
+
+--
+-- Name: staff_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staff
+    ADD CONSTRAINT staff_pkey PRIMARY KEY (staffid);
+
+--
+-- Name: time_slots_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.time_slots
     ADD CONSTRAINT time_slots_pkey PRIMARY KEY (slot_id);
 
+--
+-- Name: vacations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vacations
+    ADD CONSTRAINT vacations_pkey PRIMARY KEY (vacationid);
 
 --
--- Name: appointments appointments_doctorid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.appointments
-    ADD CONSTRAINT appointments_doctorid_fkey FOREIGN KEY (doctor_id) REFERENCES public.doctors(doctor_id);
-
-
---
--- Name: appointments appointments_patientid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: appointments_appointment_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.appointments
-    ADD CONSTRAINT appointments_patientid_fkey FOREIGN KEY (patient_id) REFERENCES public.patients(patient_id);
-
+    ADD CONSTRAINT appointments_appointment_id_fkey FOREIGN KEY (appointment_id) REFERENCES public.appointments(appointment_id);
 
 --
--- Name: appointments appointments_slot_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: appointments_doctor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.appointments
+    ADD CONSTRAINT appointments_doctor_id_fkey FOREIGN KEY (doctor_id) REFERENCES public.doctors(doctor_id);
+
+--
+-- Name: appointments_patient_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.appointments
+    ADD CONSTRAINT appointments_patient_id_fkey FOREIGN KEY (patient_id) REFERENCES public.patients(patient_id);
+
+--
+-- Name: appointments_slot_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.appointments
     ADD CONSTRAINT appointments_slot_id_fkey FOREIGN KEY (slot_id) REFERENCES public.time_slots(slot_id);
 
-
 --
--- Name: old_time_slots old_time_slots_doctorid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.old_time_slots
-    ADD CONSTRAINT old_time_slots_doctorid_fkey FOREIGN KEY (doctor_id) REFERENCES public.doctors(doctor_id);
-
-
---
--- Name: time_slots time_slots_doctorid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.time_slots
-    ADD CONSTRAINT time_slots_doctorid_fkey FOREIGN KEY (doctor_id) REFERENCES public.doctors(doctor_id) ON DELETE CASCADE;
-
-
---
--- Name: promotions promotions_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.promotions
-    ADD CONSTRAINT promotions_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid) ON DELETE CASCADE;
-
-
---
--- Name: raises raises_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: raises_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.raises
-    ADD CONSTRAINT raises_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid) ON DELETE CASCADE;
-
+    ADD CONSTRAINT raises_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid);
 
 --
--- Name: requests requests_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: requests_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.requests
-    ADD CONSTRAINT requests_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid) ON DELETE CASCADE;
-
+    ADD CONSTRAINT requests_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid);
 
 --
--- Name: staff staff_managerid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: staff_managerid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.staff
     ADD CONSTRAINT staff_managerid_fkey FOREIGN KEY (managerid) REFERENCES public.staff(staffid);
 
-
 --
--- Name: vacations vacations_staffid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: time_slots_doctor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.vacations
-    ADD CONSTRAINT vacations_staffid_fkey FOREIGN KEY (staffid) REFERENCES public.staff(staffid) ON DELETE CASCADE;
-
+ALTER TABLE ONLY public.time_slots
+    ADD CONSTRAINT time_slots_doctor_id_fkey FOREIGN KEY (doctor_id) REFERENCES public.doctors(doctor_id);
 
 --
 -- PostgreSQL database dump complete
 --
-
